2a9b3fc1b218d7c8d9ebd4d7cf2a520558b06513
Ilja Kröker <ilja.kroeker@iws.uni-stuttgart.de>
2019-10-04 15:58:02 +0200

    IO with pickle

--- py/utils.py
+++ py/utils.py
@@ -1,6 +1,7 @@
 import numpy as np
 import math
 from scipy.special import comb
+import pickle
 
 def genMultiIdx(No,dim):
     """ 
@@ -24,9 +25,30 @@ def genMultiIdx(No,dim):
             l=l+1
     return Alphas
 
+# Data format for roots, weights and details: DictName(src,aNr,Nri)
+
+def storeDataDict(Dict,fname,dir="../data"):
+    """ stores dictionary in {dir}/{fname}.p using pickle """
+    file=dir +"/" + fname + '.p'
+    print(file)
+    f=open(file,"wb")
+    pickle.dump(Dict,f)
+    f.close()
+
+def loadData(fname,dir="../data"):
+    """ load picle stored data from {dir}/{fname}.p """
+    file=dir +"/" + fname + '.p'
+    f=open(file,"rb")
+    Dict=pickle.load(f)
+    f.close()
+    return Dict
+
 if __name__=="__main__":
     print("test utils.py")
     No=3
     dim=2
     Alphas=genMultiIdx(No,dim)
     print(Alphas)
+    storeDataDict('D',"abc")
+    A=loadData("abc")
+    print(A)
