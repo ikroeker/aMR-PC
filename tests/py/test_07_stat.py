e76a7f55b872896882024501d522676b599ab51c
Ilja Kröker <ilja.kroeker@iws.uni-stuttgart.de>
2020-08-11 19:29:00 +0200

    style

--- tests/py/test_07_stat.py
+++ tests/py/test_07_stat.py
@@ -18,8 +18,6 @@ STD = 0.2
 NUMBER_OF_REALIZATIONS = 15
 
 
-
-
 def gen_observations(eval_points, params):
     """
     simple example of synthetic physical model response for testing purpose
@@ -49,21 +47,21 @@ def test_scalar_norm_likelihood():
     response_surface = observations + np.sin(points*30)/10
     likelihood = st.cmp_norm_likelihood_core(observations, response_surface, cov_mx)
     lh_cf = st.cmp_norm_likelihood_cf(STD, NUMBER_OF_MEASURMENTS)
-    
+
     norm_likelihood = norm.pdf(observations, response_surface, STD)
     #print(likelihood, lh_cf, lh_cf*likelihood)
     assert abs(lh_cf*likelihood - norm_likelihood.prod()) < EPS
-    
+
 def test_vector_norm_likelihood():
     points = np.linspace(0, 1, NUMBER_OF_MEASURMENTS)
     synthetic_parameters = np.array([0, 0])
     cov_mx = STD*STD* np.eye(NUMBER_OF_MEASURMENTS)
     observations = gen_observations(points, synthetic_parameters)
-    
+
     vec_observations = np.zeros([NUMBER_OF_MEASURMENTS, NUMBER_OF_REALIZATIONS])
     vec_response_surface = np.zeros([NUMBER_OF_MEASURMENTS, NUMBER_OF_REALIZATIONS])
     ref_likelihood = np.zeros(NUMBER_OF_REALIZATIONS)
-    
+
     for i in range(NUMBER_OF_REALIZATIONS):
         vec_observations[:, i] = observations
         vec_response_surface[:, i] = observations + np.sin(points*30)/(10+i)
