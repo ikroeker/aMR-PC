e4839ad5be767cf55a29b58b68191b89dba16b27
Ilja Kröker <ilja.kroeker@iws.uni-stuttgart.de>
2025-07-09 17:38:46 +0200

    cholesky vs pseudoinverse

--- aMRPC/datatools.py
+++ aMRPC/datatools.py
@@ -1919,12 +1919,13 @@ def gen_amrpc_dec_mk_ls(data, pol_vals, mk2sid, **kwargs):
     else:
         cov_mode = 0
 
-    alpha_mask = np.ones(p_max, dtype=np.bool_)
     cf_ls_4mkeys = {}
 
     for mkey, sids in mk2sid.items():
         if mask_dict is not None:
             alpha_mask = mask_dict[mkey]
+        else:
+            alpha_mask = np.ones(p_max, dtype=np.bool_)
         cov_mask = np.multiply.outer(alpha_mask, alpha_mask)
         # print(alpha_mask)
 
@@ -1972,23 +1973,31 @@ def gen_amrpc_dec_mk_ls(data, pol_vals, mk2sid, **kwargs):
                     # v_ls = np.linalg.pinv(phi) @ data[sids, dt_idx_x]
                     v_ls = P_inv @ data[sids, dt_idx_x]
                     cf_ls_4mk[alpha_mask, idx_x] = v_ls   
-            # elif method in ('unbias', 'pinvt', 'pinvth'):
-            #     mx_inv = np.linalg.pinv(phi.T @ phi) @ phi.T
-            #     # _eps = 1.0e-11
-            #     # _s = phi.shape[1]
-            #     # print("phi.shape:", phi.shape, _s)
-            #     # mx_inv = np.linalg.inv(np.linalg.cholesky(phi.T @ phi
-            #     #                                           + np.eye(_s)*_eps))
-            #     # mx_inv = np.linalg.inv(np.linalg.cholesky(phi.T @ phi))
-            #     # mx_inv = mx_inv.T @ mx_inv @ phi.T
-            #     for idx_x in range(x_len):
-            #         dt_idx_x = x_start + idx_x
-            #         v_ls = mx_inv @ data[sids, dt_idx_x]
-            #         # v_ls = P_inv @ phi.T @ data[sids, dt_idx_x]
-            #         cf_ls_4mk[alpha_mask, idx_x] = v_ls   
+            elif method in ('unbias_chol'):
+                # mx_inv = np.linalg.pinv(phi.T @ phi) @ phi.T
+                # _eps = 1.0e-12
+                # _s = phi.shape[1]
+                # print("phi.shape:", phi.shape, _s)
+                # mx_inv = np.linalg.inv(np.linalg.cholesky(phi.T @ phi
+                                                          # + np.eye(_s)*_eps))
+                # mx_inv = np.linalg.inv(np.linalg.cholesky(phi.T @ phi))
+                # mx_inv = mx_inv.T @ mx_inv @ phi.T
+                try:
+                    mx_inv = np.linalg.inv(np.linalg.cholesky(phi.T @ phi))
+                    mx_inv = mx_inv.T @ mx_inv @ phi.T
+                except:
+                    mx_inv =  np.linalg.pinv(phi.T @ phi) @ phi.T
+                for idx_x in range(x_len):
+                    dt_idx_x = x_start + idx_x
+                    v_ls = mx_inv @ data[sids, dt_idx_x]
+                    # v_ls = P_inv @ phi.T @ data[sids, dt_idx_x]
+                    cf_ls_4mk[alpha_mask, idx_x] = v_ls   
             elif method == 'reg_n':
-                P_inv = np.linalg.inv(np.linalg.cholesky(1/sigma_n * phi.T @ phi))
-                P_inv = P_inv.T @ P_inv 
+                try:
+                    P_inv = np.linalg.inv(np.linalg.cholesky(1/sigma_n * phi.T @ phi))
+                    P_inv = P_inv.T @ P_inv 
+                except:
+                    P_inv = np.linalg.pinv(1/sigma_n * phi.T @ phi)
                 for idx_x in range(x_len):
                     dt_idx_x = x_start + idx_x
                     v_ls = (P_inv @ phi.T / sigma_n_sq
